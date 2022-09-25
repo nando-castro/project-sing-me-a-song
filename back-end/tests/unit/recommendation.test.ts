81d8c0f70b35ee04427fc966f32e8cdf0497b731
nando-castro <fernando.castro10.master@gmail.com>
2022-09-25 13:18:37 -0300

    feat: test create recommendation conflict

--- back-end/tests/unit/recommendation.test.ts
+++ back-end/tests/unit/recommendation.test.ts
@@ -29,5 +29,24 @@ describe("Test suite insert recommendation", () => {
     await recommendationService.insert(recommendation);
     expect(recommendationRepository.create).toBeCalled();
   });
-  
+  it("Should create recommendation names must be unique", async () => {
+    const recommendation: CreateRecommendationData = {
+      name: faker.lorem.words(2),
+      youtubeLink:
+        "https://www.youtube.com/watch?v=7DRFjThtC14&ab_channel=%ED%94%BC%EC%8B%9D%EB%8C%80%ED%95%99PsickUniv",
+    };
+
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => {
+        return "data";
+      });
+
+    const response = recommendationService.insert(recommendation);
+    expect(response).rejects.toEqual({
+      type: "conflict",
+      message: "Recommendations names must be unique",
+    });
+    expect(recommendationRepository.create).not.toBeCalled();
+  });
 });
