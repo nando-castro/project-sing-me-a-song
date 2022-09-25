75cc2060947288a85f6ce2f816cbe1fd02bfe839
nando-castro <fernando.castro10.master@gmail.com>
2022-09-24 21:48:46 -0300

    feat: test config

---
+++ back-end/tests/app.test.ts
@@ -0,0 +1,14 @@
+import { jest } from "@jest/globals";
+import { recommendationService } from "../src/services/recommendationsService";
+import { recommendationRepository } from "../src/repositories/recommendationRepository";
+
+jest.mock("../src/repositories/recommendationRepository");
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  jest.clearAllMocks();
+});
+
+describe("", () => {
+  it("", async () => {});
+});
