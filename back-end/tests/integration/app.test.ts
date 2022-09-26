e95a6bfd713aff92502b2c300afd8c0ebaf35966
nando-castro <fernando.castro10.master@gmail.com>
2022-09-26 09:00:58 -0300

    feat: test integration create recommendation error cases

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -24,4 +24,24 @@ describe("Test POST/recommendation", () => {
     expect(result.status).toBe(201);
     expect(recommendationCreated).not.toBeNull();
   });
+  it("Should not create a recommendation and return status code 422", async () => {
+    const result = await supertest(app).post("/recommendations").send();
+
+    expect(result.status).toBe(422);
+  });
+  it("Should create a recommendation already exists and return status code 409", async () => {
+    await supertest(app).post("/recommendations").send(recommendationData);
+
+    const result = await supertest(app)
+      .post("/recommendations")
+      .send(recommendationData);
+
+    const recommendationCreated = await prisma.recommendation.findFirst({
+      where: {
+        name: recommendationData.name,
+      },
+    });
+    expect(result.status).toBe(409);
+    expect(recommendationCreated).not.toBeNull();
+  });
 });
