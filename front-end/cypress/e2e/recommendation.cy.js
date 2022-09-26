6064451b6b836f7210f05e0f5e80b285ab065dcf
nando-castro <fernando.castro10.master@gmail.com>
2022-09-26 17:48:40 -0300

    feat: test front cypress recommendation create

--- front-end/cypress/e2e/recommendation.cy.js
+++ front-end/cypress/e2e/recommendation.cy.js
@@ -1,5 +1,26 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+import { faker } from "@faker-js/faker";
+
+const URL = "http://localhost:3000";
+
+describe("Test Home", () => {
+  it("Should create recommendation", () => {
+    const recommendationData = {
+      name: faker.lorem.words(2),
+      youtubeLink: "https://youtu.be/7DRFjThtC14",
+    };
+    cy.visit(URL);
+
+    cy.get("#name").type(recommendationData.name);
+    cy.get("#link").type(recommendationData.youtubeLink);
+
+    cy.intercept("POST", `${URL}/recommendations`).as("createRecommendations");
+    cy.intercept("GET", `${URL}/recommendations`).as("getRecommendations");
+
+    cy.get("#button").click();
+
+    cy.wait("@createRecommendations");
+    cy.wait("@getRecommendations");
+    cy.contains(recommendationData.name).should("be.visible");
+    cy.end();
   });
 });
